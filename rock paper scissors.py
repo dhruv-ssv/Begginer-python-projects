f07275a685dc040f7d774fff26e52acc4784e455
Yegor <viryutinyegor@gmail.com>
2022-12-24 13:06:41 +0200

    improvements v1.1

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -7,60 +7,28 @@ player = input("Rock, paper, or scissors? ").lower()
 if player == cpu:
     print("Tie!")
     answer = input("Do you want to play again? y/n: ").lower()
-    if answer == "y":
+    while answer == "y":
+        cpu = random.choice(choices)
         player = input("Rock, paper, or scissors? ").lower()
+        if player == cpu:
+            print("Tie!")
+        elif player == "rock":
+            if cpu == "paper":
+                print("You lose!")
+            else:
+                print("You win!")
+        elif player == "paper":
+            if cpu == "scissors":
+                print("You lose!")
+            else:
+                print("You win!")
+        elif player == "scissors":
+            if cpu == "rock":
+                print("You lose!")
+            else:
+                print("You win!")
+        else:
+            print("Invalid input")
+        answer = input("Do you want to play again? y/n: ").lower()
     else:
-        print("Thanks for playing!")
-elif player == "rock":
-    if cpu == "scissors":
-        print("You win!")
-        answer = input("Do you want to play again? y/n: ").lower()
-        if answer == "y":
-            player = input("Rock, paper, or scissors? ").lower()
-        else:
-            print("Thanks for playing!")
-    else:
-        print("You lose!")
-        print(input("Do you want to play again? y/n: "))
-        answer = input("Do you want to play again? y/n: ").lower()
-        if answer == "y":
-            player = input("Rock, paper, or scissors? ").lower()
-        else:
-            print("Thanks for playing!")
-elif player == "paper":
-    if cpu == "rock":
-        print("You win!")
-        answer = input("Do you want to play again? y/n: ").lower()
-        if answer == "y":
-            player = input("Rock, paper, or scissors? ").lower()
-        else:
-            print("Thanks for playing!")
-    else:
-        print("You lose!")
-        answer = input("Do you want to play again? y/n: ").lower()
-        if answer == "y":
-            player = input("Rock, paper, or scissors? ").lower()
-        else:
-            print("Thanks for playing!")
-elif player == "scissors":
-    if cpu == "paper":
-        print("You win!")
-        answer = input("Do you want to play again? y/n: ").lower()
-        if answer == "y":
-            player = input("Rock, paper, or scissors? ").lower()
-        else:
-            print("Thanks for playing!")
-    else:
-        print("You lose!")
-        answer = input("Do you want to play again? y/n: ").lower()
-        if answer == "y":
-            player = input("Rock, paper, or scissors? ").lower()
-        else:
-            print("Thanks for playing!")
-else:
-    print("Invalid input!")
-    answer = input("Do you want to play again? y/n: ").lower()
-    if answer == "y":
-        player = input("Rock, paper, or scissors? ").lower()
-    else:
-        print("Thanks for playing!")
+        print("Goodbye!")
